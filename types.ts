a4c8fb7f03d311b3daa59b402dbc3432a78356d8
kk-tny <kkinoshita@toneya.co.jp>
2026-04-27 09:37:10 +0900

    Update types.ts

--- types.ts
+++ types.ts
@@ -13,7 +13,7 @@ export interface TranscriptionResult {
   callerName: string;
   subjectName: string;
   responderNames: string[];
-  inquiryType: '訃報' | '事前相談' | '自身の事前相談' | '参列問い合わせ' | '供花、供物の注文依頼' | 'その他';
+  inquiryType: '訃報' | '事前相談' | '自身の事前相談' | '参列問い合わせ' | '供花、供物の注文依頼' | '間違い電話（葬儀相談）' | '間違い電話（葬儀相談以外）' | '間違い電話（葬儀社以外宛）' | 'その他';
   details: string[];
   transcription: TranscriptionSegment[];
 }
